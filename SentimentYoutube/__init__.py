70a9763bad0608c639872efb6a334b162be475e3
Vakihito <victor.kamada.tomita@gmail.com>
2020-10-18 18:31:43 -0300

    ?

--- SentimentYoutube/__init__.py
+++ SentimentYoutube/__init__.py
@@ -2,4 +2,5 @@ from .frame_caption  import frame_caption
 from .caption_struct  import caption_struct
 from .frame_struct  import frame_struct
 from .FrameExtractor  import FrameExtractor
+from .PythiaDemo  import PythiaDemo
 from .test import test
