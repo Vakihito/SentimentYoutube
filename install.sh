5feea44ac063dc2c2b5bf21db22e6f7a0cf4d5a5
Vakihito <victor.kamada.tomita@gmail.com>
2020-11-01 20:41:41 -0300

    correct file to drive

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ mv /content/pythia /content/vqa-maskrcnn-benchmark
 
 #download my trained modelss
 cd /content
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1eBOKuaC32sZMzxZDNMUi9zWQZCzgED1i' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1eBOKuaC32sZMzxZDNMUi9zWQZCzgED1i" -O trained_models.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZGIaBKmHa7C0qmxorgU53xnH-_twBNH2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ZGIaBKmHa7C0qmxorgU53xnH-_twBNH2" -O trained_models.zip && rm -rf /tmp/cookies.txt
 unzip trained_models.zip
 
 pip3 install yacs
